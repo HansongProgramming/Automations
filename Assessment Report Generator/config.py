9131f514a1af57f282e05779154d41f92004b71f
HansongProgramming <hlf1850@students.uc-bcf.edu.ph>
2026-01-16 16:37:33 +0800

    omg wtf

--- Assessment Report Generator/config.py
+++ Assessment Report Generator/config.py
@@ -1,7 +1,7 @@
 import os
 
-# OPENAI_API_KEY = "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"
-OPENAI_API_KEY = "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"
+# OPENAI_API_KEY = "your-api-key-here"
+OPENAI_API_KEY = "api key"
 OPENAI_API_URL = "https://api.openai.com/v1/responses"
 MODEL = "gpt-4o"
 START_KEYWORD = "Media Summary"
