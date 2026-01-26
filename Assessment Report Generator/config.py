2728e5230304f97428945084846eec0997a54815
HansongProgramming <hlf1850@students.uc-bcf.edu.ph>
2026-01-26 15:35:03 +0800

    repository changes

--- Assessment Report Generator/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# OPENAI_API_KEY = "your-api-key-here"
-OPENAI_API_KEY = "api key"
-OPENAI_API_URL = "https://api.openai.com/v1/responses"
-MODEL = "gpt-4o"
-START_KEYWORD = "Media Summary"
-STOP_KEYWORD = "Media Summary Photos"
